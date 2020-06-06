7925b0c18cea2339e102d4bda327d894a4333817
eankeen <24364012+eankeen@users.noreply.github.com>
2020-06-06 08:28:08 -0700

    wip: partially create release script
    this needs to be done because lerna doesn't quite work on pnpm based
    monorepos (that have the 'workspace' protocol)

---
+++ release.sh
@@ -0,0 +1,48 @@
+#!/bin/sh -au
+
+# usage
+# ./release
+
+require_clean_work_tree () {
+	# update the index
+	git update-index -q --ignore-submodules --refresh
+	err=0
+
+	# disallow unstaged changes in the working tree
+	if ! git diff-files --quiet --ignore-submodules --
+	then
+		echo "cannot $1: you have unstaged changes."
+		git diff-files --name-status -r --ignore-submodules -- >&2
+		err=1
+	fi
+
+	# disallow uncommitted changes in the index
+	if ! git diff-index --cached --quiet HEAD --ignore-submodules --
+	then
+		echo "cannot $1: your index contains uncommitted changes."
+		git diff-index --cached --name-status -r --ignore-submodules HEAD -- >&2
+		err=1
+	fi
+
+	if [ $err = 1 ]
+	then
+		echo "Please commit or stash them."
+		exit 1
+	fi
+}
+
+require_clean_work_tree \continue
+
+packageJsonFiles=$(ls **/**/package.json | grep -v node_modules)
+for packageJsonFile in $packageJsonFiles; do
+	echo $packageJsonFile
+	sed -i "s/\"workspace:/\"/g" $packageJsonFile
+	pushd $(dirname $packageJsonFile)
+	echo $PWD
+	npm publish
+	popd 1>/dev/null
+	git checkout HEAD -- $packageJsonFile
+	echo -e "\033[0;94mdone.\033[0m\n"
+done
+
+sed -i "s/\"workspace:/\"/g" packages/fox-plugin-package-json-sort/package.json
--- version.sh
+++ version.sh
@@ -1,9 +1,13 @@
-#!/bin/sh
+#!/bin/sh -au
 
 # usage
 # ./version.sh 0.2.0
+
+test -z $1 && echo -e "\033[0;31mversion cannot be empty. exiting.\033[0m" && exit 1
 
 packageJsonFiles=$(ls **/**/package.json | grep -v node_modules)
 for packageJsonFile in $packageJsonFiles; do
 	sed -i "s/\"version\".*/\"version\": \"${1}\",/g" $packageJsonFile
 done
+
+
