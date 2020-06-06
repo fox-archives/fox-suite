d0d6bb640c4f2d66dc620f7d3ed6d4a482bda0b0
eankeen <24364012+eankeen@users.noreply.github.com>
2020-06-06 16:27:18 -0700

    fix: version script

--- version.sh
+++ version.sh
@@ -3,11 +3,10 @@
 # usage
 # ./version.sh 0.2.0
 
-test -z $1 && echo -e "\033[0;31mversion cannot be empty. exiting.\033[0m" && exit 1
+test -z ${1-} && echo -e "\033[0;31mversion cannot be empty. exiting.\033[0m" && exit 1
 
 packageJsonFiles=$(ls **/**/package.json | grep -v node_modules)
 for packageJsonFile in $packageJsonFiles; do
 	sed -i "s/\"version\".*/\"version\": \"${1}\",/g" $packageJsonFile
+	sed -Ei "s/(fox-.*)(\"\^.*)/\1\"^${1}\",/g" $packageJsonFile
 done
-
-
