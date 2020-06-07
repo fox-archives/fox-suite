ae0dd2c510b33379396f69ae453b141bd0377bbe
eankeen <24364012+eankeen@users.noreply.github.com>
2020-06-06 19:10:55 -0700

    fix: shell check version script

--- version.sh
+++ version.sh
@@ -3,12 +3,14 @@
 # usage
 # ./version.sh 0.2.0
 
-test -z ${1-} && echo -e "\033[0;31mversion cannot be empty. exiting.\033[0m" && exit 1
+# shellcheck disable=SC2039
+test -z "${1-}" && echo -e "\033[0;31mversion cannot be empty. exiting.\033[0m" && exit 1
 
-packageJsonFiles=$(echo $(ls **/**/package.json | grep -v node_modules) test/package.json)
+# shellcheck disable=SC2046,SC2116,SC2010
+packageJsonFiles=$(echo $(ls -- **/**/package.json | grep -v node_modules) test/package.json)
 for packageJsonFile in $packageJsonFiles; do
-	sed -i "s/\"version\".*/\"version\": \"${1}\",/g" $packageJsonFile
-	sed -Ei "s/(fox-.*\")(workspace.*)/\1workspace:\^${1}\",/g" $packageJsonFile
-	sed -Ei "s/(.*-fox\": \")(workspace.*)/\1workspace:\^${1}\",/g" $packageJsonFile
-	sed -Ei "s/(babel-plugin-fox-.*\")(workspace.*)/\1workspace:^${1}\",/g" $packageJsonFile
+	sed -i "s/\"version\".*/\"version\": \"${1}\",/g" "$packageJsonFile"
+	sed -Ei "s/(fox-.*\")(workspace.*)/\1workspace:\^${1}\",/g" "$packageJsonFile"
+	sed -Ei "s/(.*-fox\": \")(workspace.*)/\1workspace:\^${1}\",/g" "$packageJsonFile"
+	sed -Ei "s/(babel-plugin-fox-.*\")(workspace.*)/\1workspace:^${1}\",/g" "$packageJsonFile"
 done
