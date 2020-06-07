5b24c61b94a4f9506180d90b5f24adfdd31566bc
eankeen <24364012+eankeen@users.noreply.github.com>
2020-06-06 18:51:54 -0700

    refactor: rename fox-test to fox-test runner
    this also fixes version.sh yet again

--- version.sh
+++ version.sh
@@ -5,10 +5,10 @@
 
 test -z ${1-} && echo -e "\033[0;31mversion cannot be empty. exiting.\033[0m" && exit 1
 
-packageJsonFiles=$(ls **/**/package.json | grep -v node_modules)
+packageJsonFiles=$(echo $(ls **/**/package.json | grep -v node_modules) test/package.json)
 for packageJsonFile in $packageJsonFiles; do
 	sed -i "s/\"version\".*/\"version\": \"${1}\",/g" $packageJsonFile
 	sed -Ei "s/(fox-.*)(\"\^.*)/\1\"^${1}\",/g" $packageJsonFile
-	sed -Ei "s/(.*-fox)(\"\^.*)/\1\"^${1}\",/g" $packageJsonFile
+	sed -Ei "s/(.*-fox\": \")(\^.*)/\1\^${1}\",/g" $packageJsonFile
 	sed -Ei "s/(babel-plugin-fox-.*)(\"\^.*)/\1\"^${1}\",/g" $packageJsonFile
 done
