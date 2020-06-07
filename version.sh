a1b61c02362010b43a37e2ccd05c7445c426120d
eankeen <24364012+eankeen@users.noreply.github.com>
2020-06-06 19:02:03 -0700

    fix: ensure workspace protocol is being used

--- version.sh
+++ version.sh
@@ -8,7 +8,7 @@ test -z ${1-} && echo -e "\033[0;31mversion cannot be empty. exiting.\033[0m" &&
 packageJsonFiles=$(echo $(ls **/**/package.json | grep -v node_modules) test/package.json)
 for packageJsonFile in $packageJsonFiles; do
 	sed -i "s/\"version\".*/\"version\": \"${1}\",/g" $packageJsonFile
-	sed -Ei "s/(fox-.*)(\"\^.*)/\1\"^${1}\",/g" $packageJsonFile
-	sed -Ei "s/(.*-fox\": \")(\^.*)/\1\^${1}\",/g" $packageJsonFile
-	sed -Ei "s/(babel-plugin-fox-.*)(\"\^.*)/\1\"^${1}\",/g" $packageJsonFile
+	sed -Ei "s/(fox-.*\")(workspace.*)/\1workspace:\^${1}\",/g" $packageJsonFile
+	sed -Ei "s/(.*-fox\": \")(workspace.*)/\1workspace:\^${1}\",/g" $packageJsonFile
+	sed -Ei "s/(babel-plugin-fox-.*\")(workspace.*)/\1workspace:^${1}\",/g" $packageJsonFile
 done
