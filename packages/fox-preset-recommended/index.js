20904e63f88d12e74d36ab62abc55e6ff6116c0c
eankeen <24364012+eankeen@users.noreply.github.com>
2020-06-13 20:52:46 -0700

    feat: add recommended preset

---
+++ packages/fox-preset-recommended/index.js
@@ -0,0 +1,13 @@
+const r = require.resolve.bind(null);
+
+/** @type {import("fox-types").IPresetExportIndex} */
+const preset = {
+	plugins: [
+		r('fox-plugin-eslint'),
+		r('fox-plugin-markdownlint'),
+		r('fox-plugin-prettier'),
+		r('fox-plugin-stylelint'),
+	],
+};
+
+export default preset;
