f877d8b80bbb9ad31c7007800186cb18d9a43da8
eankeen <24364012+eankeen@users.noreply.github.com>
2020-06-14 02:30:04 -0700

    types: fix invalid import

--- packages/fox-plugin-sort-package-json/src/bin.ts
+++ packages/fox-plugin-sort-package-json/src/bin.ts
@@ -1,11 +1,11 @@
 import * as foxUtils from 'fox-utils'
-import { formatFunction } from '.'
+import { fixFunction } from '../src'
 import { info } from './info'
 
 export async function cli() {
 	await foxUtils.buildCli(process.argv, {
 		pluginName: info.name,
 		pluginDescription: info.description,
-		fixFunction: formatFunction,
-	})
+		fixFunction: fixFunction,
+	});
 }
