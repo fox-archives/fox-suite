41131d7192ef0a119d58746c6f213e1bb30237e7
eankeen <24364012+eankeen@users.noreply.github.com>
2020-06-14 22:33:00 -0700

    fix: version.sh to work with entries in last key of array

--- version.sh
+++ version.sh
@@ -10,10 +10,10 @@ test -z "${1-}" && echo -e "\033[0;31mversion cannot be empty. exiting.\033[0m" 
 packageJsonFiles=$(echo $(ls -- **/**/package.json | grep -v node_modules) test/package.json package.json)
 for packageJsonFile in $packageJsonFiles; do
 	sed -i "s/\"version\".*/\"version\": \"${1}\",/g" "$packageJsonFile"
-	sed -Ei "s/(fox-.*\")(workspace.*)/\1workspace:\^${1}\",/g" "$packageJsonFile"
-	sed -Ei "s/(.*-fox\": \")(workspace.*)/\1workspace:\^${1}\",/g" "$packageJsonFile"
-	sed -Ei "s/(babel-plugin-fox-.*\")(workspace.*)/\1workspace:^${1}\",/g" "$packageJsonFile"
+	sed -Ei "s/(fox-.*\")(workspace.*\")(.*)/\1workspace:\^${1}\"\3/g" "$packageJsonFile"
+	sed -Ei "s/(.*-fox\": \")(workspace.*\")(.*)/\1workspace:\^${1}\"\3/g" "$packageJsonFile"
+	sed -Ei "s/(babel-plugin-fox-.*\")(workspace.*\")(.*)/\1workspace:^${1}\"\3/g" "$packageJsonFile"
 done
 
-git commit -a -m "release: v${1-}"
-git push origin dev
+# git commit -a -m "release: v${1-}"
+# git push origin dev
