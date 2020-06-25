62f05b760ae2f0a63cbfc0a26db87a89a1cfac56
eankeen <24364012+eankeen@users.noreply.github.com>
2020-06-25 15:32:43 -0700

    release: v1.2.1

--- version.sh
+++ version.sh
@@ -7,7 +7,7 @@
 test -z "${1-}" && echo -e "\033[0;31mversion cannot be empty. exiting.\033[0m" && exit 1
 
 # shellcheck disable=SC2046,SC2116,SC2010
-packageJsonFiles=$(echo $(ls -- **/**/package.json | grep -v node_modules) test/package.json package.json)
+packageJsonFiles=$(echo $(ls -- **/**/package.json | grep -v node_modules) test/package.json package.json lerna.json)
 for packageJsonFile in $packageJsonFiles; do
 	sed -i "s/\"version\".*/\"version\": \"${1}\",/g" "$packageJsonFile"
 	sed -Ei "s/(fox-.*\")(workspace.*\")(.*)/\1workspace:\^${1}\"\3/g" "$packageJsonFile"
