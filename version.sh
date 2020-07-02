61d3b45e012d752c02ff6ce28054e4c82fa64cac
eankeen <24364012+eankeen@users.noreply.github.com>
2020-07-01 17:04:20 -0700

    fix: version script to build project

--- version.sh
+++ version.sh
@@ -1,10 +1,14 @@
-#!/bin/sh -au
+#!/bin/sh -eu
 
 # usage
 # ./version.sh 0.2.0
 
 # shellcheck disable=SC2039
 test -z "${1-}" && echo -e "\033[0;31mversion cannot be empty. exiting.\033[0m" && exit 1
+
+cd "packages/fox-suite"
+pnpm run build
+cd ../..
 
 # shellcheck disable=SC2046,SC2116,SC2010
 packageJsonFiles=$(echo $(ls -- **/**/package.json | grep -v node_modules) test/package.json package.json lerna.json)
